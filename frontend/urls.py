cc37ca141520da6b40beebc02ee25d4035aea1ce
Dishant Shah <dishantshah3133@gmail.com>
2024-02-24 19:52:21 +0530

    Setup

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from frontend import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('frontend.urls')),
+    path('',views.index,name='home'),
 ]
